3566835da2c0e740cfe0c67cc4c22cff4f7628a1
Per Hansen <me@perhp.com>
2025-02-19 20:40:56 +0100

    chore: format sketch

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,10 +1,10 @@
 /**
  * Refactored example using DFRobot ENS160 and DFRobot BME280.
- * 
+ *
  * Make sure you have installed the required libraries:
  *   - DFRobot_ENS160
  *   - DFRobot_BME280
- * 
+ *
  * Connections (typical I2C):
  *   - VCC to 3.3V/5V (depending on sensor board specs)
  *   - GND to GND
@@ -29,22 +29,23 @@ DFRobot_BME280_IIC bme(&Wire, 0x76);
  */
 void printBMEStatus(DFRobot_BME280_IIC::eStatus_t eStatus)
 {
-  switch(eStatus) {
-    case DFRobot_BME280_IIC::eStatusOK:
-      Serial.println(F("Everything OK"));
-      break;
-    case DFRobot_BME280_IIC::eStatusErr:
-      Serial.println(F("Unknown error"));
-      break;
-    case DFRobot_BME280_IIC::eStatusErrDeviceNotDetected:
-      Serial.println(F("Device not detected"));
-      break;
-    case DFRobot_BME280_IIC::eStatusErrParameter:
-      Serial.println(F("Parameter error"));
-      break;
-    default:
-      Serial.println(F("Unknown status"));
-      break;
+  switch (eStatus)
+  {
+  case DFRobot_BME280_IIC::eStatusOK:
+    Serial.println(F("Everything OK"));
+    break;
+  case DFRobot_BME280_IIC::eStatusErr:
+    Serial.println(F("Unknown error"));
+    break;
+  case DFRobot_BME280_IIC::eStatusErrDeviceNotDetected:
+    Serial.println(F("Device not detected"));
+    break;
+  case DFRobot_BME280_IIC::eStatusErrParameter:
+    Serial.println(F("Parameter error"));
+    break;
+  default:
+    Serial.println(F("Unknown status"));
+    break;
   }
 }
 
@@ -57,7 +58,8 @@ void setup()
   // Initialize the BME280 sensor
   bme.reset();
   Serial.println(F("Initializing BME280 sensor..."));
-  while (bme.begin() != DFRobot_BME280_IIC::eStatusOK) {
+  while (bme.begin() != DFRobot_BME280_IIC::eStatusOK)
+  {
     Serial.println(F("BME280 init failed"));
     printBMEStatus(bme.lastOperateStatus);
     delay(2000);
@@ -67,7 +69,8 @@ void setup()
 
   // Initialize ENS160 sensor
   Serial.println(F("Initializing ENS160 sensor..."));
-  while (ens160.begin() != NO_ERR) {
+  while (ens160.begin() != NO_ERR)
+  {
     Serial.println(F("ENS160 init failed, please check wiring"));
     delay(3000);
   }
@@ -102,9 +105,9 @@ void loop()
 
   // Read ENS160 gas sensor data
   uint8_t ensStatus = ens160.getENS160Status();
-  uint8_t aqi       = ens160.getAQI();
-  uint16_t tvoc     = ens160.getTVOC();
-  uint16_t eco2     = ens160.getECO2();
+  uint8_t aqi = ens160.getAQI();
+  uint16_t tvoc = ens160.getTVOC();
+  uint16_t eco2 = ens160.getECO2();
 
   // Print ENS160 readings
   Serial.println(F("======== ENS160 Readings ========"));
