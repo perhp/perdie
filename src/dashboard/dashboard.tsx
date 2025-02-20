433c68e83009db51507069b1b3e96fae0a511380
Per Hansen <me@perhp.com>
2025-02-20 21:02:54 +0100

    feat: setup routing

---
+++ src/dashboard/dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <h1 className="text-amber-900 text-5xl font-bold my-4 leading-tight">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,9 +1,19 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
+import About from "./about/about";
+import Dashboard from "./dashboard/dashboard";
+import "./index.css";
 
 function start() {
   const root = createRoot(document.getElementById("root")!);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 if (document.readyState === "loading") {
