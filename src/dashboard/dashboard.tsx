c45eaabe45b96e58cb62c7994187f7c286e73e89
Per Hansen <me@perhp.com>
2025-02-22 20:09:59 +0100

    feat: change dashboard

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <h1 className="text-amber-900 text-5xl font-bold my-4 leading-tight">
+      <h1 className="text-amber-950 text-5xl font-bold my-4 leading-tight">
         Dashboard
       </h1>
     </div>
