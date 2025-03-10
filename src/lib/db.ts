39523dc7badc4f235f3da0ce3c28ca83b28e33bb
Per Hansen <me@perhp.com>
2025-03-10 19:56:23 +0100

    feat: prepare for storing usages

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import { Database } from "bun:sqlite";
 
 export const db = new Database(":memory:");
+
 db.exec(`
     CREATE TABLE IF NOT EXISTS climate_readings (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,6 +13,23 @@ db.exec(`
         aqi REAL,
         tvoc REAL,
         eco2 REAL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS usage (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cpu_temperature REAL,
+        cpu_usage REAL,
+        uptime INTEGER,
+        memory_total REAL,
+        memory_used REAL,
+        memory_free REAL,
+        memory_shared REAL,
+        memory_buff_cache REAL,
+        memory_available REAL,
+        voltage REAL,
         createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
 `);
--- src/models/usage.model.ts
+++ src/models/usage.model.ts
@@ -1,16 +1,12 @@
 export interface Usage {
-  cpu: {
-    temperature: number;
-    usage: number;
-  };
+  cpu_temperature: number;
+  cpu_usage: number;
   uptime: number;
-  memory: {
-    total: number;
-    used: number;
-    free: number;
-    shared: number;
-    buffCache: number;
-    available: number;
-  };
+  memory_total: number;
+  memory_used: number;
+  memory_free: number;
+  memory_shared: number;
+  memory_buffCache: number;
+  memory_available: number;
   voltage: number;
 }
